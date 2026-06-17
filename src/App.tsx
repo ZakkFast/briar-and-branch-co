d685fefaf5f43844945db2929ac3f715810e2f75
Zakk Fast <zakkfast@Mac.attlocal.net>
2026-06-17 16:55:19 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="min-h-screen bg-green-950 text-amber-50 grid place-items-center">
+      <h1 className="text-5xl font-bold">Briar & Branch Co.</h1>
+    </main>
+  );
+}
